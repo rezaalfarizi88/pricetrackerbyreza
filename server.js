c03c1316fc3c81958c0de476bdc3a75124f1d6d9
rezaalfarizi88 <rezaalfaproject@gmail.com>
2026-06-01 09:58:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -165,6 +165,7 @@ app.use(session({
 }));
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use('/images', express.static(path.join(__dirname, "images")));
 
 app.get("/", (req, res) => {
   res.redirect("/login.html");
